4c65ad3ea2b4c6ce301fcb2ad117e3a355cf09b4
raul <calcas0731@gmail.com>
2021-09-03 08:26:39 -0500

    fixes in deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t calcas0731/multi-client:latest -t calcas0731/multi-client:$SHA -f ./client/Dockerfile .client
-docker build -t calcas0731/multi-server:latest -t calcas0731/multi-server:$SHA -f ./server/Dockerfile .server
-docker build -t calcas0731/multi-worker:latest -t calcas0731/multi-worker:$SHA -f ./worker/Dockerfile .worker
+docker build -t calcas0731/multi-client-k8s:latest -t calcas0731/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t calcas0731/multi-server-k8s:latest -t calcas0731/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t calcas0731/multi-worker-k8s:latest -t calcas0731/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push calcas0731/multi-client:latest
-docker push calcas0731/multi-server:latest
-docker push calcas0731/multi-worker:latest
+docker push calcas0731/multi-client-k8s:latest
+docker push calcas0731/multi-server-k8s:latest
+docker push calcas0731/multi-worker-k8s:latest
 
-docker push calcas0731/multi-client:$SHA
-docker push calcas0731/multi-server:$SHA
-docker push calcas0731/multi-worker:$SHA
+docker push calcas0731/multi-client-k8s:$SHA
+docker push calcas0731/multi-server-k8s:$SHA
+docker push calcas0731/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=calcas0731/multi-server:$SHA
-kubectl set image deployments/client-deployment client=calcas0731/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=calcas0731/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=calcas0731/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=calcas0731/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=calcas0731/multi-worker-k8s:$SHA
