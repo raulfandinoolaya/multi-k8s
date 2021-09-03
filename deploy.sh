45b332955948c43162abb4bbcb3b7c1fd28ab348
raul <calcas0731@gmail.com>
2021-09-03 08:18:36 -0500

    fix docker account in deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile .client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile .server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile .worker
+docker build -t calcas0731/multi-client:latest -t calcas0731/multi-client:$SHA -f ./client/Dockerfile .client
+docker build -t calcas0731/multi-server:latest -t calcas0731/multi-server:$SHA -f ./server/Dockerfile .server
+docker build -t calcas0731/multi-worker:latest -t calcas0731/multi-worker:$SHA -f ./worker/Dockerfile .worker
 
 docker push calcas0731/multi-client:latest
 docker push calcas0731/multi-server:latest
@@ -11,7 +11,6 @@ docker push calcas0731/multi-server:$SHA
 docker push calcas0731/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
-
+kubectl set image deployments/server-deployment server=calcas0731/multi-server:$SHA
+kubectl set image deployments/client-deployment client=calcas0731/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=calcas0731/multi-worker:$SHA
